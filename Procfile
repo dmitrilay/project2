7fd0ecac5845f824c78b27bd4461d56502b7fcdc
Dmitriy Nozhkin <dmitrilay@gmail.com>
2020-09-15 22:28:17 +0600

    Start

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn locallibrary.wsgi --log-file -
+web: gunicorn project2.wsgi --log-file -
--- project2/settings.py
+++ project2/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i^blfxy#z9)0%#rdhnb9!w=pe**j=uua7y3$n*#*-%c6$dt$+m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
